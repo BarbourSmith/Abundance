7dcc4a61ab216cd96719ed02988d3420dfdcc3ce
Tristan <tristan.huber.206@gmail.com>
2025-08-11 15:00:42 -0700

    Fix issue where clicking on tag molecule triggered recalculation of all downstream elements

--- src/molecules/tag.js
+++ src/molecules/tag.js
@@ -90,10 +90,12 @@ export default class Tag extends Atom {
       label: "Add Tag",
       disabled: false,
       onChange: (value) => {
-        this.tags = [];
-        this.tags.push(value); // Add the new tag to the array
-        this.name = this.tags.toString();
-        this.onUpstreamChange();
+        const newVal = [value];
+        if (this.name !== newVal.toString()) {
+          this.tags = newVal;
+          this.name = newVal.toString();
+          this.onUpstreamChange();
+        }
       },
     };
 
@@ -105,7 +107,8 @@ export default class Tag extends Atom {
   /**
    * Compute the tagged geometry.
    */
-  async compute(inputs) {
+  compute(inputs) {
+    console.trace(`${this.uniqueID} compute called with inputs:`, inputs);
     const inputID = inputs.geometry;
     const tags = this.tags;
     return GlobalVariables.cad.tag(this.uniqueID, inputID, tags);
