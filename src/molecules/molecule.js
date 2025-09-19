c1215b700904fed910bd84e29f800ea37ef49957
Tristan <tristan.huber.206@gmail.com>
2025-09-19 15:23:39 -0700

    current molecule should call back it's selfSubscriber so that rendering works as expected

--- src/molecules/molecule.js
+++ src/molecules/molecule.js
@@ -854,6 +854,7 @@ export default class Molecule extends Atom {
       // This is the output of the currently focused molecule
       // don't dispatch changes upstream because those entities aren't
       // shown.
+      this.selfSubscriber();
       return;
     }
     super.propagateChange();
@@ -1154,7 +1155,7 @@ export default class Molecule extends Atom {
           let newID = GlobalVariables.generateUniqueID();
           idPairs[oldID] = newID;
           atom.uniqueID = newID;
-          
+
           // Recursively process any nested atoms (e.g., within GitHubMolecules)
           processNestedAtoms(atom);
         });
@@ -1188,10 +1189,10 @@ export default class Molecule extends Atom {
           }
         });
       }
-      
+
       // Process connectors in nested atoms recursively
       if (obj.allAtoms) {
-        obj.allAtoms.forEach(atom => processConnectors(atom));
+        obj.allAtoms.forEach((atom) => processConnectors(atom));
       }
     };
 
